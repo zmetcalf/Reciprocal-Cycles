65c9a43a9f35515c23639d2bb209d41e72bb2366
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-03-24 19:53:58 -0600

    Starting work with BigDecimal

--- ReciprocalCycles.java
+++ ReciprocalCycles.java
@@ -1,7 +1,13 @@
+import java.math.BigDecimal;
+
 public class ReciprocalCycles {
 
-	public ReciprocalCycles {
+	private BigDecimal testDeci;
+	private BigDecimal numerator = new BigDecimal(1.0);
+	private BigDecimal denominator = new BigDecimal(7.0);
 
+	public ReciprocalCycles() {
+		testDeci = numerator.divide(denominator, 1000, BigDecimal.ROUND_FLOOR);
+		System.out.println(testDeci);
 	}
-
 }
--- ReciprocalCyclesTest.java
+++ ReciprocalCyclesTest.java
@@ -1,7 +1,7 @@
 public class ReciprocalCyclesTest {
 	
 	public static void main(String [] args) {
-
+		ReciprocalCycles test = new ReciprocalCycles();
 	}
 
 }
