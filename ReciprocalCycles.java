255e92bc50938a677b3c1547b299ddc7e481dc48
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-03-29 17:35:30 -0600

    Functional, needs less precision, faster computer

--- ReciprocalCycles.java
+++ ReciprocalCycles.java
@@ -3,18 +3,33 @@ import java.math.BigDecimal;
 
 public class ReciprocalCycles {
 
+	private int recipAmount;
+    private int precision = 100000;
+    	
 	private BigDecimal testDeci;
-	private BigDecimal numerator = new BigDecimal(1.0);
-	private BigDecimal denominator = new BigDecimal(7.0);
-
+	private BigDecimal highestDeci = new BigDecimal(0.0);
+	private BigDecimal denominator = new BigDecimal(2.0);
+	
+	private static final BigDecimal NUMERATOR = new BigDecimal(1.0);	
+	private static final BigDecimal UPTO = new BigDecimal(1000.0);
+	private static final BigDecimal COUNTER = new BigDecimal(1.0);
+	
 	public ReciprocalCycles() {
-		try {
-		    testDeci = numerator.divide(denominator, BigDecimal.ROUND_UNNECESSARY);
+		while(denominator.compareTo(UPTO) == -1) {
+		    try {
+		        testDeci = NUMERATOR.divide(denominator, BigDecimal.ROUND_UNNECESSARY);
+		    }
+		    catch(ArithmeticException e) {
+		        testDeci = NUMERATOR.divide(denominator, precision, BigDecimal.ROUND_DOWN);
+		        recipAmount = findReciprocal(testDeci);
+		    
+		        if(BigDecimal.valueOf(recipAmount).compareTo(highestDeci) == 1) {
+		            highestDeci = denominator;
+		        }
+		    }
+		    denominator = denominator.add(COUNTER);
 		}
-		catch(ArithmeticException e) {
-		    testDeci = numerator.divide(denominator, 10000, BigDecimal.ROUND_DOWN);
-		    System.out.println(findReciprocal(testDeci));
-		}
+		System.out.println(highestDeci);
 	}
 	
 	private int findReciprocal(BigDecimal test) {
@@ -22,17 +37,19 @@ public class ReciprocalCycles {
 	    char[] characterArray;
 		char[] testArray1;
 		char[] testArray2;
-	    int counter = 0;
+	    int counter = 1;
 		int endNumber = 0;
 	    
 	    characterArray = (test.toString()).toCharArray();
 
-		while(counter < 5000) {
-			for(int i =  counter; i < (5000 - counter); i++) {
+        if(characterArray[2] == characterArray[3]) {
+            return 1;
+        }
+
+		while(counter < (precision / 2)) {
+			for(int i =  counter; i < ((precision / 2) - counter - 5); i++) {
 				testArray1 = Arrays.copyOfRange(characterArray, counter + 2, counter + 2 + i);
 				testArray2 = Arrays.copyOfRange(characterArray, counter + 3 + i, counter + 3 + i + i);
-				System.out.println(testArray1);
-				System.out.println(testArray2);
 				if(Arrays.equals(testArray1, testArray2)) {
 					return (i + 1);
 				}
